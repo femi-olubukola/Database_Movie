4170ea889519a9222c14fd0f42da4f1c1dbf8a56
Oluwafemi Samuel <meetfemisamuel@yahoo.com>
2020-07-21 03:06:51 +0100

    MovieDB

---
+++ Movie Table Insertion.sql
@@ -0,0 +1,91 @@
+use movie;
+insert into movie
+values ('1', 'Eyes Wide Shut', '1999', '08', 'English', '1999-01-01', 'Lagos');
+insert into movie
+values ('2', 'American Beauty', '2001', '06', 'French', '2001-02-10', 'Ivory');
+insert into movie
+values ('3', 'Booze', '1997', '12', 'Arab', '1997-09-05', 'Texas');
+insert into movie
+values ('4', 'Freeze', '1998', '05', 'Chinese', '1998-12-31', 'Italy');
+
+
+insert into actor
+values ('1', 'John', 'Naat', 'M');
+insert into actor
+values ('2', 'Fred', 'Holo', 'M');
+insert into actor
+values ('3', 'Tria', 'Ann', 'F');
+insert into actor 
+values ('4', 'Anna', 'Viloski', 'F');
+
+
+insert into director
+values ('1', 'Fredrick', 'Lamar');
+insert into director
+values ('2', 'Anthony', 'Bas');
+insert into director
+values ('3', 'Cindy', 'Alaska');
+insert into director
+values ('4', 'Dare', 'Alade');
+
+
+insert into genres
+values ('1', 'Comedy');
+insert into genres
+values ('2', 'Epic');
+insert into genres
+values ('3', 'Romance');
+insert into genres
+values ('4', 'Action');
+
+
+insert into movie_cast
+values ('1', '1', 'dog');
+insert into movie_cast
+values ('2', '2', 'cat');
+insert into movie_cast
+values ('3', '3', 'rat');
+insert into movie_cast
+values ('4', '4', 'chicken');
+
+
+insert into movie_direction
+values ('1', '1');
+insert into movie_direction
+values ('2', '2');
+insert into movie_direction
+values ('3', '3');
+insert into movie_direction
+values ('4', '4');
+
+
+insert into movie_genres
+values ('1', '1');
+insert into movie_genres
+values ('2', '2');
+insert into movie_genres
+values ('3', '3');
+insert into movie_genres
+values ('4', '4');
+
+insert into reviewer
+values ('1', 'Joe');
+insert into reviewer
+values ('2', 'Biden');
+insert into reviewer
+values ('3', 'Trump');
+insert into reviewer
+values ('4', 'Baracko');
+
+
+insert into rating
+values ('1', '1', '7', '4');
+insert into rating
+values ('2', '2', '5', '8');
+insert into rating
+values ('3', '3', '3', '1');
+insert into rating
+values ('4', '4', '8', '6');
+
+
+
